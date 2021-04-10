a72775a60a767a39b662912763462eef79317666
eunsu <holicholicpop@gmail.com>
2021-04-10 16:04:40 +0900

    210410 수정

--- js/main.js
+++ js/main.js
@@ -42,14 +42,23 @@
     
 
     
-    //해더 함수
+       //사용할 변수 선언
     const header = document.querySelector('#header'),
     main = document.querySelector('main'),
     body = document.body;
-    
+    const introduce = document.querySelector('#introduce'),
+    profile = document.querySelector('#profile'),
+    publishing = document.querySelector('#publishing'),
+    samples = document.querySelector('#sample'),
+    puLeftBtn = document.querySelector('.pu-left-btn'),
+    puRightBtn = document.querySelector('.pu-right-btn'),
+    puImgSlide = document.querySelector('.pu-slide'),
+    puSlideItem = document.querySelector('.pu-slideBox'),
+    naviList = document.querySelectorAll('#header nav ul li'),
+    section = document.querySelectorAll('section');
 
-    function headerFunc(a,b){
-        
+    //해더 함수
+    function headerFunc(){
         fadeOut(header,100);
         setTimeout(function(){
         fadeIn(header,100);
@@ -60,16 +69,37 @@
             document.body.classList.add('sticky');
         },2500)
     }
+ 
+    //네비 클릭 시 해당 섹션이동, 네비 클래스 넣기
+    function headerNaviFunc(){
 
-    const introduce = document.querySelector('#introduce'),
-    profile = document.querySelector('#profile'),
-    publishing = document.querySelector('#publishing'),
-    samples = document.querySelector('#sample'),
-    puLeftBtn = document.querySelector('.pu-left-btn'),
-    puRightBtn = document.querySelector('.pu-right-btn'),
-    puImgSlide = document.querySelector('.pu-slide'),
-    puSlideItem = document.querySelector('.pu-slideBox');
+    //클릭했을 때 스크롤 이동, 클래스 부여
+    //네비 클릭 했을 때 할 함수 
+    for(let i=0 ; i<naviList.length ; i++){
+        const navi = naviList[i]; // i: 0-> about me, 1-> web p.., 2-> samp.., 3->
+        navi.addEventListener('click',function(e){
+            // naviList[i].classList.remove();
+            // naviList.forEach((n, i) => { // ES6의 고차함수 (map, forEach, reduce, ...)
+            //     n.classList.remove('naviCurrent');
+            // });
+            for (let ii = 0; ii < naviList.length; ii++) {
+                const n = naviList[ii];
+                n.classList.remove('naviCurrent');
+            }
+            // 1. 네비 리스트가 갖고 있는 클래스 지우기
+            e.preventDefault();
+            section[i+1].scrollIntoView({ block: 'end',  behavior: 'smooth' });
+            //2. 클릭 했을 때 각각의 섹션으로 이동
+            this.classList.add('naviCurrent');
+            //3. 현재 클릭한 this의 클래스 부여
+        })
+    }
+    }
+    headerFunc();
+    headerNaviFunc();
 
+
+    //퍼블리싱 클릭 시 article이동
     let slideWidth = puSlideItem.clientWidth;
     let i=1;
     
@@ -86,14 +116,10 @@
         console.log(i);
     })
 
-    let puIdx = 1;
-
-    
-
 
 
     
-    //인트로 클릭 시 인트로 페이지 이동
+    //인트로, 이력서 클릭 시 페이지 이동
 
     const gotoIntroBtn = document.querySelector('.gotoIntro');
     gotoIntroBtn.addEventListener('click',function(){
@@ -104,17 +130,43 @@
     gotoResume.addEventListener('click',function(){
         location.href='./resume/index.html';
     })
-    headerFunc();
+
+
     //해더 함수 호출
     const cloudTitle = document.querySelector('#sample h2');
     const clouds = document.querySelector('.clouds');
     //구름 만들기 위한 html elements
+    function changeBg(){
+
+        const images = [
+            'url("./images/bg1.jpg")',
+            'url("./images/bg2.jpg")',
+            'url("./images/bg3.jpg")',
+            'url("./images/bg4.jpg")',
+            'url("./images/bg5.jpg")',
+            'url("./images/bg6.jpg")',
+            'url("./images/bg7.jpg")',
+            'url("./images/bg8.jpg")',
+            'url("./images/bg9.jpg")',
+            'url("./images/bg10.jpg")',
+        ]
+
+        const section = document.querySelector('#contact .img-container')
+        const bg = images[Math.floor(Math.random() * images.length)];
+        section.style.backgroundImage = bg;
+    }
+
+   let timer = setInterval(changeBg, 1000);
+
+
     // 스크롤 이벤트
     window.addEventListener('scroll',function(){
+                //현재 스크롤 값
         let currentScroll = window.scrollY;
-        //현재 스크롤 값
-        console.log(currentScroll);
 
+        /*1. intro : 0 , 2. profile : 970, 3. pub : 1936 , 4:sample : 2902 +- 시켜서 자연스럽게 이어지도록 설정
+         */
+        // 해더 스크롤
         if(currentScroll>=100){
             header.style.backgroundColor = 'rgba(0,0,0,0.3)';
             //해더 스크롤 했을 때, 현재 스크롤 위치가 100 이상이면 배경색 변경
@@ -122,69 +174,36 @@
             header.style.backgroundColor = 'inherit';
             //100 이하이면 배경은 inherit
         }
-        if(currentScroll>280){
+
+
+        // intro 스크롤에 따라 클래스 제어
+        if(currentScroll>750){
             document.querySelector('.move-area').classList.remove('active');
+            document.querySelector('.code').classList.remove('code-ani');
             
         } else{
             document.querySelector('.move-area').classList.add('active');
-        }
-
-        if(currentScroll>=500){
-            document.querySelector('.code').classList.remove('code-ani');
-            profile.classList.add('show');
-        }
-        if(currentScroll<350){
-            profile.classList= 'remove';
             document.querySelector('.code').classList.add('code-ani');
-            
         }
 
-        if(currentScroll>1208){
+        // profile 행성 클래스 제어
+        if(currentScroll>=750&& currentScroll<1936){
+            profile.classList.add('show');
+        } else{
             profile.classList= 'remove';
         }
-        if(currentScroll>=1925&&currentScroll<3207) {
+
+        //samplese 구름 클래스, 타이틀 제어
+        if(currentScroll>=1836&&currentScroll<3102){
             clouds.classList.add('cloudsAni');
             cloudTitle.style.marginBottom = currentScroll-1050 *2 +'px';
-            console.log(currentScroll);
-            
         } else{
             clouds.classList.add('removeAni');
             clouds.classList.remove('cloudsAni');
-           
         }
 
     })
 
- 
-    
-    function headerNaviFunc(){
-        const naviList = document.querySelectorAll('#header nav ul li'),
-        section = document.querySelectorAll('section');
-    //클릭했을 때 스크롤 이동, 클래스 부여
 
-    //네비 클릭 했을 때 할 함수 
-    for(let i=0 ; i<naviList.length ; i++){
-        const navi = naviList[i]; // i: 0-> about me, 1-> web p.., 2-> samp.., 3->
-        navi.addEventListener('click',function(e){
-            // naviList[i].classList.remove();
-            // naviList.forEach((n, i) => { // ES6의 고차함수 (map, forEach, reduce, ...)
-            //     n.classList.remove('naviCurrent');
-            // });
-            for (let ii = 0; ii < naviList.length; ii++) {
-                const n = naviList[ii];
-                n.classList.remove('naviCurrent');
-            }
-            // 1. 네비 리스트가 갖고 있는 클래스 지우기
-            e.preventDefault();
-            section[i].scrollIntoView({ block: 'end',  behavior: 'smooth' });
-            //2. 클릭 했을 때 각각의 섹션으로 이동
-            this.classList.add('naviCurrent');
-            //3. 현재 클릭한 this의 클래스 부여
-        })
-
-    }
-    }
-
-    headerNaviFunc();
 
 })();
